2bb234ac584f4254e442de97a2a8bb9448b0b62f
Dominic St-Pierre <dominicstpierre@gmail.com>
2021-10-31 13:30:18 -0400

    added missing API functions (all sudo*, cache, password reset)

--- src/backend.ts
+++ src/backend.ts
@@ -1,6 +1,21 @@
 import * as fetch from "node-fetch";
 import * as formData from "form-data";
 
+export interface ListParam {
+	page: number;
+	size: number;
+	descending: boolean;
+}
+
+export interface EmailData {
+	fromName: string;
+	from: string;
+	to: string;
+	subject: string;
+	body: string;
+	replyTo: string;
+}
+
 export class Backend {
 	private baseURL: string = "https://na1.staticbackend.com";
 	private pubKey: string = "";
@@ -11,6 +26,9 @@ export class Backend {
 		if (region) {
 			if (region == "dev") {
 				this.baseURL = "http://localhost:8099";
+			} else if region.indexOf(".") {
+				// for self-hosted base URL
+				this.baseURL = region;
 			} else {
 				this.baseURL = `https://${region}.staticbackend.com`;
 			}
@@ -67,20 +85,54 @@ export class Backend {
 		return await this.req("", "POST", "/login", body);
 	}
 
+	async changePassword(token: string, email: string, oldPass: string, newPass: string) {
+		const body = { email: email, oldPassword: oldPass, newPassword: newPass};
+		return await this.req(token, "POST", "/user/changepw", body);
+	}
+
+	async getPasswordResetCode(rootToken: string, email: string) {
+		const path = `/password/resetcode?${encodeURIComponent(email)}`;
+		return await this.req(rootToken, "GET", path);
+	}
+
+	async resetPassword(email: string, code: string, newPass: string) {
+		const body = {email: email, code: code, password: newPass};
+		return await this.req("", "POST", "/password/reset", body);
+	}
+
+	async sudoGetToken(rootToken: string, accountId: string) {
+		return await this.req(rootToken, "GET", `/sudogettoken/${accountId}`);
+	}
+
+	async cacheGet(rootToken: string, key: string) {
+		return await this.req(rootToken, "GET", `/sudo/cache?key=${key}`);
+	}
+
+	async cacheSet(rootToken: string, key: string, value: any) {
+		const body = {key: key, value: JSON.stringify(value)};
+		return await this.req(rootToken, "POST", "/sudo/cache", body);
+	}
+
 	async create(token: string, repo: string, doc) {
 		return await this.req(token, "POST", `/db/${repo}`, doc)
 	}
 
-	async list(token: string, repo: string) {
-		return await this.req(token, "GET", `/db/${repo}`);
+	async createBulk(token: string, repo: string, docs: Array<any>) {
+		return await this.req(token, "POST", `/db/${repo}?bulk=1`, docs);
+	}
+
+	async list(token: string, repo: string, param?: ListParam) {
+		const qs = this.listParamToQuerystring(param);
+		return await this.req(token, "GET", `/db/${repo}${qs}`);
 	}
 
 	async getById(token: string, repo: string, id: string) {
 		return await this.req(token, "GET", `/db/${repo}/${id}`);
 	}
 
-	async query(token: string, repo: string, filters) {
-		return await this.req(token, "POST", `/query/${repo}`, filters);
+	async query(token: string, repo: string, filters, param?: ListParam) {
+		const qs = this.listParamToQuerystring(param);
+		return await this.req(token, "POST", `/query/${repo}${qs}`, filters);
 	}
 
 	async update(token: string, repo: string, id: string, doc) {
@@ -91,8 +143,27 @@ export class Backend {
 		return await this.req(token, "DELETE", `/db/${repo}/${id}`);
 	}
 
-	async sudoUpdate(adminToken: string, repo: string, id: string, doc) {
-		return await this.req(adminToken, "PUT", `/sudo/${repo}/${id}`, doc);
+	async sudoList(rootToken: string, repo: string, param?: ListParam) {
+		const qs = this.listParamToQuerystring(param);
+		return await this.req(rootToken, "GET", `/sudo/${repo}${qs}`);
+	}
+
+	async sudoGetById(rootToken: string, repo: string, id: string) {
+		return await this.req(rootToken, "GET", `/sudo/${repo}/${id}`);
+	}
+
+	async sudoUpdate(rootToken: string, repo: string, id: string, doc) {
+		return await this.req(rootToken, "PUT", `/sudo/${repo}/${id}`, doc);
+	}
+
+	async sudoQuery(rootToken: string, repo: string, filters, param?: ListParam) {
+		const qs = this.listParamToQuerystring(param);
+		return await this.req(token, "POST", `/sudoquery/${repo}${qs}`, filters);
+	}
+
+	async increase(token: string, repo: string, id: string, field: string, n: number) {
+		const body = {field: field, range: n};
+		return await this.req(token, "PUT", `/inc/${repo}/${id}`, body);
 	}
 
 	async storeFile(token: string, size: number, buf: ArrayBuffer) {
@@ -104,5 +175,24 @@ export class Backend {
 
 		const ct = `multipart/form-data; boundary=${fd.getBoundary()}`;
 		return await this.rawreq(ct, token, "POST", "/storage/upload", fd);
+	}
+
+	async deleteFile(token: string, id: string) {
+		return await this.req(token, "GET", `/sudostorage/delete?id=${id}`);
+	}
+
+	async sendMail(rootToken: string, data: EmailData) {
+		return await this.req(rootToken, "POST", "/sudo/sendmail", data);
+	}
+
+	private listParamToQuerystring(param?: ListParam): string {
+		var qs = "";
+		if (param) {
+			qs = `?page=${param.page}&size=${param.size}`;
+			if (param.descending) {
+				qs += "&desc=true";
+			}
+		}
+		return qs;
 	}
 }
